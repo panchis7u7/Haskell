bdf987488bfad7c7f8f4e75889b8b92c4219ef26
panchis7u7 <smadrigal.rod@gmail.com>
2021-09-20 21:42:55 -0500

    Accumulators and guards.

--- Pruebas/pruebas.hs
+++ Pruebas/pruebas.hs
@@ -3,52 +3,77 @@ squared :: Integer -> Integer
 squared n = n * 2
 
 -- | bool inRange(int min. int max, int x){}
-inRange :: Integer -> Integer -> Integer -> Bool 
+inRange :: Integer -> Integer -> Integer -> Bool
 inRange min max x = x >= min && x <= max
 
 -- | bool inRangeTwo(int min. int max, int x){} LazyEval
-inRangeTwo :: Integer -> Integer -> Integer -> Bool 
-inRangeTwo min max x = 
+inRangeTwo :: Integer -> Integer -> Integer -> Bool
+inRangeTwo min max x =
     let inLowerBound = min <= x
         inUpperBound = max >= x
     in
         inLowerBound && inUpperBound
 
 -- | bool inRangeThree(int min. int max, int x){} LazyEval
-inRangeThree :: Integer -> Integer -> Integer -> Bool 
+inRangeThree :: Integer -> Integer -> Integer -> Bool
 inRangeThree min max x = ilb && iub
     where
         ilb = min <= x
         iub = max >= x
 
 -- | bool inRangeFour(int min. int max, int x){} LazyEval
-inRangeFour :: Integer -> Integer -> Integer -> Bool 
-inRangeFour min max x = 
-    if ilb then iub else False 
+inRangeFour :: Integer -> Integer -> Integer -> Bool
+inRangeFour min max x =
+    if ilb then iub else False
     where
         ilb = min <= x
         iub = max >= x
 
 -- | Recursion with if then.
-factorial:: Integer -> Integer 
+factorial:: Integer -> Integer
 factorial n =
     if n <= 1 then
         1
     else
         n * factorial (n-1)
 
-{- | Recursion with guards. Otherwise is a constant 
+{- | Recursion with guards. Otherwise is a constant
     that always evaluates to True.-}
-factorialGuards:: Integer -> Integer 
-factorialGuards n 
+factorialGuards:: Integer -> Integer
+factorialGuards n
     | n <= 1 = 1
     | otherwise = n * factorial (n-1)
 
-{- | Recursion with Accumulators. La funcion factorialAccum 
+{- | Recursion with Accumulators. La funcion factorialAccum
 es el resultado de una funcion auxiliar aux, acc is the accumulator -}
-factorialAccum:: Integer -> Integer 
+factorialAccum:: Integer -> Integer
 factorialAccum n = aux n 1
     where
         aux n acc
             | n <= 1 = acc
             | otherwise = aux (n-1) (n*acc)
+
+fiboGuards:: Integer -> Integer
+fiboGuards n
+    | n == 0 = 0
+    | n == 1 = 1
+    | otherwise = fiboGuards (n-1) + fiboGuards (n-2)
+
+fiboAlt:: Integer -> Integer
+fiboAlt 0 = 0
+fiboAlt 1 = 1
+fiboAlt n = fiboAlt(n-1) + fiboAlt(n-2)
+
+{- Very fast fibonacci implementation. -}
+fiboAcc :: Int -> Int
+fiboAcc n = fib 0 1 n
+    where fib a b n
+              | n <= 1 = b
+              | otherwise = fib b (a+b) (n-1)
+
+{- # LANGUAGE BangPatterns # -XBangPatterns -}
+fibfast :: Int -> Int
+fibfast n = fib' 0 1 n
+    where fib' a !b !n
+              | n <= 1 = b
+              | otherwise = fib' b (a+b) (n-1)
